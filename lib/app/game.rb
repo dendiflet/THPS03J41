d976e95ecf08252f105e644b9729150181dd8a7c
dendiflet <bretin.iannis@gmail.com>
2019-01-25 03:17:15 +0100

    do the turn, mic mac de variables

--- lib/app/game.rb
+++ lib/app/game.rb
@@ -2,7 +2,7 @@
 
 class Games
 
-  attr_accessor :j1, :j2, :j1m, :j2m, :starter, :cases_played, :case, :player, :turn
+  attr_accessor :j1, :j2, :j1m, :j2m, :starter, :cases_played, :acase, :player, :turn
 
   def initialize(j1_name, j2_name)
   	@turn = 0
@@ -10,7 +10,7 @@ class Games
   	@j2 = j2_name
   	@j1m = Array.new
   	@j2m = Array.new
-  	@cases = Array.new
+  	@case = Array.new
   	@player = Array.new
   	@starter = Array.new
   	@cases_played = Array.new
@@ -48,14 +48,19 @@ class Games
 		else
 		  case_sized = imput
 		end
-#binding.pry
-    # ajoute la casse proposée a celles déja jouée
-    cases_allready_played << case_sized #ajout de la val sinon trop de 
-    #binding.pry
 
- 	  if cases_allready_played.uniq! == nil   #si pas en double uniq! renvoye nil
+
+
+
+cases_allready_played_copy = cases_allready_played + ["#{case_sized}"]
+      
       #binding.pry 
- 	    return good_imput = {:ok => true, :case => case_sized}
+
+    # compare la casse proposée a l'array de celles déja jouée
+
+ 	  if cases_allready_played_copy.uniq! == nil   #si pas en double uniq! renvoye nil
+      #binding.pry 
+ 	    return good_imput = {:ok => true, :case_just_played_ok => case_sized}
  	  else
  	   	puts "case deja jouée, essaye une autre case\n"
  	   	return good_imput = {:ok => false}
@@ -63,6 +68,22 @@ class Games
   end
 
 
+  #maintenant que la acase est vérifier, faut faire les modifs
+  def do_the_turn(just_played_case, game_var)
+  	binding.pry
+		game_var.cases_played << just_played_case
+		@j1="yaya",
+		@j2="beber",
+		game_var.turn += 1
 
+		case game_var.player
+		when game_var.j1
+			game_var.j1m << just_played_case
+		when game_var.j2
+			game_var.jm2 << just_played_case
+		end
 
+  end
+
+# fin de class
 end
--- lib/controller.rb
+++ lib/controller.rb
@@ -15,35 +15,31 @@ class Controller
   	@interface = Interface.new
   	#params = hash
   	params = @interface.start
-  	binding.pry
   	@new_game = Games.new(params[:j1], params[:j2])
 
-#binding.pry
+    partie
 
-   partie
   end
 
   def partie
 
+    while @new_game.turn < 9
 
-  unless @new_game.turn > 9
+    	#va afficher le morpion et demander/vérifier la saisie
+      good_imput = { :ok => false }
+      while good_imput[:ok] == false
+  	    #binding.pry
+        @interface.show(@new_game.j1m, @new_game.j2m)
+              #binding.pry 
 
-  good_imput = { :ok => false }
+        imput = @interface.ask(@new_game.cases_played, @new_game.player)
+              #binding.pry 
 
-  while good_imput[:ok] == false
-  	binding.pry
-    @interface.show(@new_game.j1m, @new_game.j2m)
-    imput = @interface.ask(@new_game.cases_played, @new_game.player)
-    good_imput = @new_game.imput_verification(imput, @new_game.cases_played)
-  end
-
-puts "je suis passé"
- end
-
-
-
-
-
+        good_imput = @new_game.imput_verification(imput, @new_game.cases_played)
+      end
+#binding.pry
+      @new_game.do_the_turn(good_imput[:case_just_played_ok], @new_game)
+       puts "je suis passé"
 
 
 
@@ -51,16 +47,8 @@ puts "je suis passé"
 
 
 
-
-
-
-
-
-
-
-
-
-
+    #while turn 9 end
+    end
 
   #fin de partie
   end
